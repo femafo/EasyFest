323f2fdc5708d96552690d302e7b4198c181e0d9
marttantonio <150140408+marttantonio@users.noreply.github.com>
2024-05-16 10:32:01 +0200

    Delete Proyecto_1ºDAM/BDA/tablaAdmin.sql

--- Proyecto_1ºDAM/BDA/tablaAdmin.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS administrador (
-    id_administrador INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_usuario VARCHAR(50),
-    nombre VARCHAR(50),
-    apellidos VARCHAR(100),
-    correo VARCHAR(100),
-    contraseña VARCHAR(100)
-);
